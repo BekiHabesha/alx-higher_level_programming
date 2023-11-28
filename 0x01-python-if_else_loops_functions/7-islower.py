61f9454871f9453a628e7a46093d2f77ee7b7bf4
BekiHabesha <bereketdere@gmail.com>
2023-11-28 15:58:06 -0800

    7-islower.py.

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+# Author - Bereket Dereje
+
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
-	 return True
-     else:
+         return True
+    else:
          return False
