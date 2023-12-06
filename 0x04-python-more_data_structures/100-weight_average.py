1c211006a26b6415a4caec4d37f1d59c2c495a32
BekiHabesha <bereketdere@gmail.com>
2023-12-06 13:43:47 -0800

    100-main.py - 13. Weighted average.

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Author - Bereket Dereje
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    num = 0
+    deno = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        deno += tup[1]
+
+    return (num / deno)
