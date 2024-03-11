312b52dce2d7ae957f63f7120d04e5e1a68d70d0
BekiHabesha <bereketdere@gmail.com>
2024-03-11 11:54:55 -0700

    No.6. Loop to languages.

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/* Author: Bereket Dereje */
+/*
+A script that prints 3 lines:
+  (like 1-multi_languages.js)
+  but by using an array of string and a loop
+      The first line: 
+                     “C is fun”
+      The second line:
+                     “Python is cool”
+      The third line:
+                     “JavaScript is amazing”
+      Must use console.log(...) to print all output
+      Not allowed to use var
+      Not allowed to use any if/else statement
+      Can use only one console.log
+      Must use a loop (while, for, etc.)
+*/
+
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
+}
