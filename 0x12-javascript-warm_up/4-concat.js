edc90d3ebdd1ff1404f1bf91f6a148128d28f7f5
BekiHabesha <bereketdere@gmail.com>
2024-03-11 10:24:11 -0700

    No.4. Create a sentence.

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* Author: Bereket Dereje */
+/*
+A script that prints two arguments passed to it,
+  in the following format: “ is ”
+     Must use console.log(...) to print all output
+     Not allowed to use var
+*/
+
+console.log(process.argv[2] + ' is ' + process.argv[3]);
