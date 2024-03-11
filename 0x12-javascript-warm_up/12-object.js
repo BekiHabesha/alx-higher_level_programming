9cc77b17e7e740fcd7ad3c5ffba895e2c91a5759
BekiHabesha <bereketdere@gmail.com>
2024-03-11 12:19:03 -0700

    No.12. Object.

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* Author: Bereket Dereje */
+/*
+Update the given script to replacethe value 12 with 89:
+   Not allowed to use var
+*/
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89; /* MY CODE IS HERE */
+console.log(myObject);
