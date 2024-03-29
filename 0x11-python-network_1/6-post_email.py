1897aa3be262d18903b799aceb4e6af6fb957a2e
BekiHabesha <bereketdere@gmail.com>
2024-03-29 12:58:19 -0700

    No6.POST an email #1.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Author - Bereket Dereje
+"""A Phython script that:
+  - takes in a URL,
+  - sends a request to the URL and displays the value
+  - of the X-Request-Id variable found in the header ofthe response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
